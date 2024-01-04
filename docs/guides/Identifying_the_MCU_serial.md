0ececd4ba7b28e0d9f00d72349b5b8469195c832
reemo3dp <150130755+reemo3dp@users.noreply.github.com>
2024-01-04 23:19:56 +0100

    Add script

--- docs/guides/Identifying_the_MCU_serial.md
+++ docs/guides/Identifying_the_MCU_serial.md
@@ -5,8 +5,20 @@ This tutorial guides you through the steps of identifying the serial connection 
 See also https://www.klipper3d.org/Installation.html#configuring-klipper
 
 Power on the host and make sure all mcu‎s you want to use are connected and powered on. 
-[Connect to your host using SSH](https://docs.vorondesign.com/build/software/ssh.html) and run the following commands:
+[Connect to your host using SSH](https://docs.vorondesign.com/build/software/ssh.html).
 
+## Listing all MCUs
+
+klippy usually communicates with each mcu through a serial port, accessed via a tty device on linux. These devices show up in the `/dev/` folder as `/dev/ttyUSB0`. These names are not stable and might change between reboots, which is why a more stable alias is needed.
+
+The following script will list all plugged-in, initialized TTYs, among those hopefully all mcus.
+
+``` bash title="Download and run this script"
+curl -sSL https://reemo3dp.github.io/scripts/find_mcus.sh | bash
+```
+``` bash title="find_mcus.sh"
+--8<-- "docs/scripts/find_mcus.sh"
+```
 
 ## Troubleshooting
 
---
+++ docs/scripts/find_mcus.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Exit script immediately on error, uninitialized or if any piped command fails
+set -euo pipefail
+
+find_ttys() {
+    # Taken from https://unix.stackexchange.com/a/144735
+    # Enumerates all USB devices found in sysfs, finds the ones with a tty
+    # and prints the device name, the serial number and
+    # all associated links (e.g. in /dev/serial)
+    for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
+        (
+            set +u
+            syspath="${sysdevpath%/dev}"
+            devname="$(udevadm info -q name -p $syspath)"
+            [[ "$devname" == "bus/"* ]] && exit
+            eval "$(udevadm info -q property --export -p $syspath)"
+            ! [[ "$SUBSYSTEM" == "tty" ]] && exit
+            echo "$ID_SERIAL at $ID_PATH"
+            echo "/dev/$devname"
+            printf '%s\n' $DEVLINKS
+            echo ""
+        )
+    done
+}
+
+# TTYs will always be at the root level, even if udev is broken
+echo "# Root Dev TTYs"
+find /dev -maxdepth 1 \( -name 'ttyUSB*' -o -name 'ttyACM*' \)
+echo ""
+
+# Listing all `/dev/serial` symlinks helps identifying name collisions in `by-id`
+echo "# /dev/serial symlinks"
+find /dev/serial -type l -exec file {} \;
+echo ""
+
+# Printing `lsusb` potentially shows MCUs in DFU/HID mode
+echo "# lsusb"
+lsusb --tree
+echo ""
+
+# Finally, print all properly initialized TTYs
+echo "# All TTYs"
+find_ttys
