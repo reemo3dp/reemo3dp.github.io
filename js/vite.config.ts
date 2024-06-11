13a7b01e5f603b6847d54444663e7b9b2d0a2ee8
reemo3dp <150130755+reemo3dp@users.noreply.github.com>
2024-06-11 20:14:22 +0200

    Fix base path for JS

--- js/vite.config.ts
+++ js/vite.config.ts
@@ -6,6 +6,7 @@ import path, { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte({ compilerOptions: { customElement: true } })],
+  base: "/assets/js/",
   build: {
     outDir: "../docs/assets/js",
     lib: {
